b3c7ecafb0660e5a4bcedcf800179ab8e9b87bf5
Katie Clay <katie.clayy@outlook.com>
2024-04-18 21:27:49 -0500

    still working through understnading data

--- notebook/scooters_sql.sql
+++ notebook/scooters_sql.sql
@@ -1,5 +1,36 @@
-SELECT * FROM scooters LIMIT 10;
+SELECT * FROM scooters LIMIT 100;
 
 SELECT * FROM trips LIMIT 100;
 
 SELECT COUNT(sumdid) FROM scooters;
+
+
+--charge levels has nulls
+SELECT * 
+FROM scooters 
+WHERE chargelevel IS NULL;
+
+
+--date range
+SELECT MIN(pubdatetime), MAX(pubdatetime)
+FROM scooters
+
+
+--Q1.) During this period, seven companies offered scooters. How many scooters did each company have in this time frame? Did the number for each company change over time? Did scooter usage vary by company?
+
+SELECT DISTINCT companyname, COUNT(sumdid)
+FROM scooters
+GROUP BY companyname;
+
+
+SELECT  DISTINCT companyname, pubdatetime
+FROM scooters
+LIMIT 100
+
+SELECT DISTINCT companyname, COUNT(companyname), pubdatetime
+FROM scooters
+GROUP BY companyname, pubdatetime
+
+
+SELECT MONTH(pubdatetime)
+FROM scooters
